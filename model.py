6350e9684af916878116eea76f9bb191112ac6ee
Shaurya Verma <shauryaji@gmail.com>
2026-08-14 20:43:23 +1000

    Step 5: make_quotes

--- model.py
+++ model.py
@@ -111,8 +111,9 @@ def red_black_card_game_value(num_red, num_black):
 
     return {'value': value, 'stop_now': stop_now}
 
-# Step 5 - make_quotes (not yet solved)
-# TODO: implement
+# Step 5 - make_quotes
+def make_quotes(fair_value, spread_width):
+    return {'bid': fair_value - spread_width/2, 'ask': fair_value + spread_width/2}
 
 # Step 6 - execute_trade (not yet solved)
 # TODO: implement
